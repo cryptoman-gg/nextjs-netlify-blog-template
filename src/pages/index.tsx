7cf2b565522f65334f2a91344e9e74cfae7b3d1f
cryptoman-gg <79981527+cryptoman-gg@users.noreply.github.com>
2021-08-07 17:48:02 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,11 +19,17 @@ export default function Index() {
           <h2>仮想通貨を中心に投資・お得情報を発信していきます！！</h2>
           <SocialList />
           <div>
-            <a className="twitter-timeline" data-width="300" data-height="500" data-theme="dark" href="https://twitter.com/takutyan_ut?ref_src=twsrc%5Etfw">Tweets by takutyan_ut</a> <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
-            <a href="https://h.accesstrade.net/sp/cc?rk=0100o4jh00lj0h" rel="nofollow" referrerpolicy="no-referrer-when-downgrade"><img src="https://h.accesstrade.net/sp/rr?rk=0100o4jh00lj0h" alt="コインチェック" border="0" /></a>
+            <a className="twitter-timeline" data-width="300" data-height="500" data-theme="dark" href="https://twitter.com/takutyan_ut?ref_src=twsrc%5Etfw">
+              Tweets by takutyan_ut
+            </a> 
+            <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+            <a href="https://h.accesstrade.net/sp/cc?rk=0100o4jh00lj0h" rel="nofollow" referrerpolicy="no-referrer-when-downgrade">
+              <img src="https://h.accesstrade.net/sp/rr?rk=0100o4jh00lj0h" alt="コインチェック" border="0" />
+            </a>
             <a href="https://px.a8.net/svt/ejp?a8mat=3H5XC6+F2ZCHE+3250+6FHC1" rel="nofollow">
-            <img border="0" width="468" height="60" alt="" src="https://www24.a8.net/svt/bgt?aid=210228486912&wid=001&eno=01&mid=s00000014274001080000&mc=1"></a>
-            <img border="0" width="1" height="1" src="https://www14.a8.net/0.gif?a8mat=3H5XC6+F2ZCHE+3250+6FHC1" alt="">
+            <img border="0" width="468" height="60" alt="" src="https://www24.a8.net/svt/bgt?aid=210228486912&wid=001&eno=01&mid=s00000014274001080000&mc=1" />
+              </a>
+            <img border="0" width="1" height="1" src="https://www14.a8.net/0.gif?a8mat=3H5XC6+F2ZCHE+3250+6FHC1" alt="" />
           </div>
          </div>
           </div>
